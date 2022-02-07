71602e2092d468f39fcb9b36634af5de7841756c
Ray0097968 <rjm18b@my.fsu.edu>
2022-02-07 16:15:37 -0500

    Update LineTrack.h

--- RobotBaseCode/LineTrack.h
+++ RobotBaseCode/LineTrack.h
@@ -19,26 +19,54 @@ void track(void)
   //Serial.print(A2);
   //Serial.print(A3);
   //Serial.print(A4);
-  Serial.println(analogRead(A3));
+  Serial.println(analogRead(A5));
+  int x = 300;
+  int prevCom = 0;
   
-  if((analogRead(A2) > 100) && (analogRead(A3) > 100) && (analogRead(A4) > 100))
+  if(analogRead(A3) < x)
   {
-    command1 = 7;
-    command2 = 7;
+    command1 = 8;
+    command2 = 8;
   }
-    else if((analogRead(A1) > 100) && (analogRead(A2) > 100) && (analogRead(A3) > 100))
+    else if((analogRead(A1)< x) || (analogRead(A2) < x))//left
     {
-      command1 = 0;
-      command2 = 7;
-    }
-      else if((analogRead(A3) > 100) && (analogRead(A4) > 100) && (analogRead(A5) > 100))
+      if((analogRead(A1)< x))
       {
-        command1 = 7;
-        command2 = 0;
+        command1 = 7;//lefmotr
+        command2 = 9;//righmotr
+      }
+      else
+      {
+        command1 = 6;
+        command2 = 8;
+      }
+      prevCom = 1;
+    }
+      else if((analogRead(A4) < x) || (analogRead(A5) < x))//right
+      {
+        if((analogRead(A5)< x))
+        {
+        command1 = 9;
+        command2 = 7;
+        }
+        else
+        {
+          command1 = 8;
+          command2 = 6;
+        }
+        prevCom = 5;
       }
         else
         {
-          command1 = 0;
-          command2 = 0;
+          if(prevCom == 5)
+          {
+          command1 = 16;
+          command2 = 8;
+          }
+          else if (prevCom == 1)
+          {
+            command1 = 8;
+            command2 = 16;
+          }
         }
 }
