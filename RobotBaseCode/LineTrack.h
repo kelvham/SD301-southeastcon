1c0359a530ebf6491a72d6d5755ca3fccfcd0f70
Ray0097968 <rjm18b@my.fsu.edu>
2022-03-27 19:56:37 -0400

    Update LineTrack.h

--- RobotBaseCode/LineTrack.h
+++ RobotBaseCode/LineTrack.h
@@ -12,14 +12,18 @@ int Mid;
 int Mid_L;
 int L;
 int Out_L;
-float wr;
+float wr = 0;
+float prev_wr = 0;
 float LookAhead_Dis[3] = {0.1513, 0.1552, 0.1616}; //[outside L/R sens, L/R sens] in meters
 float LookAhead_Ycomp = 0.15; //[outside L/R sens, L/R sens] in meters
 float WheelDis = 0.215; //in meters
 float wheelRad = 0.0575; //in meters
-int x = 200;
+int x = 55;
 int n = 0;
 int v = 1.5;
+int index = 0;
+int sums[7];
+int readings[30][7];
 
 void track_setup(void) //enabling line tracker sensors
 {
@@ -32,16 +36,34 @@ pinMode(A5, INPUT); //left
 pinMode(A6, INPUT); //out left
 }
 
+int left;
+int right;
+
 void track(void)
 {
-  Out_L = analogRead(A7);
-  L = analogRead(A6);
-  Mid_L = analogRead(A5);
-  Mid = analogRead(A4);
-  Mid_R = analogRead(A3);
-  R = analogRead(A2);
-  Out_R = analogRead(A1);
-  /*
+  //Low Pass Filter for the sensor readings
+  left = 0;
+  right = 0;
+  int data[] = {analogRead(A7), analogRead(A6), analogRead(A5), analogRead(A4), analogRead(A3), analogRead(A2), analogRead(A1), analogRead(A0)};
+
+  for(int d = 0; d < 7; d++)
+  {
+    sums[d] = sums[d] - readings[index][d];
+    readings[index][d] = data[d];
+    sums[d] = sums[d] + readings[index][d];
+    data[d] = sums[d]/30; 
+  } 
+  index = (index + 1)%30;
+
+  //sensor values
+  Out_L = data[0];
+  L = data[1];
+  Mid_L = data[2];
+  Mid = data[3];
+  Mid_R = data[4];
+  R = data[5];
+  Out_R = data[6];
+  
   Serial.print(Out_L);
   Serial.print(" ");
   Serial.print(L);
@@ -54,9 +76,14 @@ void track(void)
   Serial.print(" ");
   Serial.print(R);
   Serial.print(" ");
-  Serial.println(Out_R);*/
+  Serial.println(Out_R);
 
-  if((Out_R > x) || (Out_L > x))
+  if((Out_R > x) && (Out_L > x) && (L > x) && (R > x))
+  {
+     command1 = 2;
+     command2 = 2;
+  }
+  else if((Out_R > x) || (Out_L > x))
     {
       if(Out_L > x)
       { 
@@ -164,82 +191,7 @@ void track(void)
         command1 = -1*((2*PI*v*wheelRad) - (WheelDis/2)*wr);
         command2 = ((2*PI*v*wheelRad) + (WheelDis/2)*wr);
         break;
-      default:
-        command1 = -0.75;//rightmotr speed
-        command2 = -0.75;//lefthmotr speed
       }
     }
-/*
-  if(Mid > x)
-  {
-    command1 = v*1.1;
-    command2 = v;
-    n = 0;
-  }
-  else if ((L > x) || (R > x))
-    {
-      if(L > x)
-      { 
-        wr = (2*(2*PI*v*wheelRad)*LookAhead_Ycomp[1])/pow(LookAhead_Dis[1],2);
-        command1 = -1*((2*PI*v*wheelRad) - (WheelDis/2)*wr);
-        command2 = ((2*PI*v*wheelRad) + (WheelDis/2)*wr);
-        n = 1;
-      }
-      else
-      {
-        wr = -1*(2*(2*PI*v*wheelRad)*LookAhead_Ycomp[1])/pow(LookAhead_Dis[1],2);
-        command1 = ((2*PI*v*wheelRad) - (WheelDis/2)*wr);
-        command2 = -1*((2*PI*v*wheelRad) + (WheelDis/2)*wr);
-        n = 2;
-      }
-    }                                                                                   
-    else if((Out_R > x) || (Out_L > x))
-    {
-      if(Out_L > x)
-      { 
-        wr = (2*(2*PI*v*wheelRad)*LookAhead_Ycomp[0])/pow(LookAhead_Dis[0],2);
-        command1 = -1*((2*PI*v*wheelRad) - (WheelDis/2)*wr);
-        command2 = ((2*PI*v*wheelRad) + (WheelDis/2)*wr);
-        n = 3;
-      }
-      else
-      {
-        wr = -1*(2*(2*PI*v*wheelRad)*LookAhead_Ycomp[0])/pow(LookAhead_Dis[0],2);
-        command1 = ((2*PI*v*wheelRad) - (WheelDis/2)*wr);
-        command2 = -1*((2*PI*v*wheelRad) + (WheelDis/2)*wr);
-        n = 4;
-      }
-    }
-   else
-    {
-      switch (n)
-      {
-      case 1: 
-        wr = (2*(2*PI*v*wheelRad)*LookAhead_Ycomp[1])/pow(LookAhead_Dis[1],2);
-        command1 = -1*((2*PI*v*wheelRad) - (WheelDis/2)*wr);
-        command2 = ((2*PI*v*wheelRad) + (WheelDis/2)*wr);
-        break;
-      case 2:
-        wr = -1*(2*(2*PI*v*wheelRad)*LookAhead_Ycomp[1])/pow(LookAhead_Dis[1],2);
-        command1 = ((2*PI*v*wheelRad) - (WheelDis/2)*wr);
-        command2 = -1*((2*PI*v*wheelRad) + (WheelDis/2)*wr);
-        break;
-      case 3:
-        wr = (2*(2*PI*v*wheelRad)*LookAhead_Ycomp[0])/pow(LookAhead_Dis[0],2);
-        command1 = -1*((2*PI*v*wheelRad) - (WheelDis/2)*wr);
-        command2 = ((2*PI*v*wheelRad) + (WheelDis/2)*wr);
-        break;
-      case 4:
-        wr = -1*(2*(2*PI*v*wheelRad)*LookAhead_Ycomp[0])/pow(LookAhead_Dis[0],2);
-        command1 = ((2*PI*v*wheelRad) - (WheelDis/2)*wr);
-        command2 = -1*((2*PI*v*wheelRad) + (WheelDis/2)*wr);
-        break;
-      default:
-        command1 = -0.75;//rightmotr speed
-        command2 = -0.75;//lefthmotr speed
-      }
-    }*/
-  Serial.print(command1);
-  Serial.print(" ");
-  Serial.println(command2);
+
 }
