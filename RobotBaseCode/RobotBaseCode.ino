98372218bce901b5e2ed45f61965b55ccbec99c2
Kelvin Hamilton <50687400+kelvham@users.noreply.github.com>
2022-03-29 17:44:56 -0400

    Update RobotBaseCode.ino

--- RobotBaseCode/RobotBaseCode.ino
+++ RobotBaseCode/RobotBaseCode.ino
@@ -18,7 +18,7 @@ void loop(void)
   track();//line follower sensors
   get_current_status();
 
-  if (current_position > 17)
+  if (current_position > 16)
   {
       des_vel[0] = 0;//leftmotor speed
       des_vel[1] = 0;//rightmotor speed
